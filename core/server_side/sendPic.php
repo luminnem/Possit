2e6443329e95f0a016e6396e9c7f1c626d6c5af2
Luminnem <jerrymrslime@gmail.com>
2014-07-20 16:42:34 +0200

    Code cleaned: Send picture function.

--- core/server_side/sendPic.php
+++ core/server_side/sendPic.php
@@ -4,14 +4,28 @@ session_start();
 
 
 $urls = $_POST['url'];
-$reply = mysql_real_escape_string($_POST["reply"]);
 
 $urls = explode("\n", $urls);
 $errors = 0;
 $total = count($urls);
 
+$to = null;
+$reply = null;
+
+//If this picture was sent to an user get the id of that user
+if (isset($_POST['to'])) {
+	$to = mysql_real_escape_string($_POST["to"]);
+}
+
+if (isset($_POST["reply"])) {
+	$reply = mysql_real_escape_string($_POST["reply"]);
+}
+
+//Get the id of the user who's sending the picture
+$by = mysql_real_escape_string($_SESSION['id']);
+
 foreach ($urls as &$url) {
-	sendPic($url, $errors, $connection);
+	sendPic($url, $errors, $connection, $to, $reply, $by);
 }
 unset($url);
 
@@ -24,34 +38,17 @@ if ($errors > 0) {
 
 
 
-function sendPic($url, &$errors, $connection) {
+function sendPic($url, &$errors, $connection, $to, $reply, $by) {
 	if (getimagesize($url)) { // Get the size of the image, if the size is equal to null or it's equal to zero then the image doesn't exist.
 		
 		//Filter possible codes
-		$pic = strip_tags($url);
-		$pic = mysql_real_escape_string($pic);
+		$pic = getEncodedPicture($url);
 		
-		//Convert strange chars
-		$pic = str_replace("\\n", ".\endl", $pic);
-		$pic = str_replace("\\'", ".\sinQuote", $pic);
-		//$pic = str_replace("%", "&#37;", $pic);
+		$query_1 = "INSERT INTO posts(user, body, type) VALUES ('$by', '$pic', '2')";
+		$q = mysql_query($query_1, $connection) or die ("Ups... problem when sending your pic");
 		
-		//Encode url
-		$pic = base64_encode($pic);
-		
-		//If this picture was sent to an user get the id of that user
-		if (isset($_POST['to'])) {
-			$to = $_POST['to'];
-			$to = mysql_real_escape_string($to);
-		}
-		
-		//Get the id of the user who's sending the picture
-		$by = mysql_real_escape_string($_SESSION['id']);
-		
-		if($reply)
+		if(!isset($reply))
 		{
-			$query_1 = "INSERT INTO posts(user, body, type) VALUES ('$by', '$pic', '2')";
-			$q = mysql_query($query_1, $connection) or die ("Ups... problem when sending your pic");
 			//If this picture was sent to an user, post it on its profile.
 			if (isset($to)) {
 				$query = "INSERT INTO users_replies(user, post) VALUES ('$to', LAST_INSERT_ID())";
@@ -62,8 +59,6 @@ function sendPic($url, &$errors, $connection) {
 		
 		else
 		{
-			$query_1 = "INSERT INTO posts(user, body, type) VALUES ('$by', '$pic', '2')";
-			$q = mysql_query($query_1, $connection) or die ("Ups... problem when sending your pic");
 			//If this picture was sent to an user, post it on its profile.
 			if (isset($to)) {
 				$query = "INSERT INTO posts_replies(post, reply) VALUES ('$to', LAST_INSERT_ID())";
@@ -75,4 +70,18 @@ function sendPic($url, &$errors, $connection) {
 		//If the image doesn't exist ...
 		$errors += 1;
 	}
+}
+
+function getEncodedPicture($url) {
+	$pic = strip_tags($url);
+	$pic = mysql_real_escape_string($pic);
+	
+	//Convert strange chars
+	$pic = str_replace("\\n", ".\endl", $pic);
+	$pic = str_replace("\\'", ".\sinQuote", $pic);
+	//$pic = str_replace("%", "&#37;", $pic);
+	
+	//Encode url
+	$pic = base64_encode($pic);
+	return $pic;
 }
